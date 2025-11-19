e0a7259f2b70e5aef4b75f8f2f071ea7996dc97b
Zeeshan Zakir <support@works360.com>
2025-11-20 01:46:14 +0500

    icon

--- app/managed-apps/page.tsx
+++ app/managed-apps/page.tsx
@@ -205,7 +205,7 @@ export default function ManagedAppsPage() {
               className="stepper-nav-btn flex items-center gap-2 text-blue-600 hover:text-blue-800"
               onClick={handleNext}
             >
-              Next <img src="/arrow.png" alt="Next" width={18} />
+              Next <img src="/Arrow.png" alt="Next" width={18} />
             </button>
           </div>
 
